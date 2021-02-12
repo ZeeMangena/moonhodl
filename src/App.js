23c0bff6ec7e8088856422ebc974ebd4dd2a2bad
ZeeMangena <zibusisomangena@gmail.com>
2021-02-12 17:11:54 +0200

    Initial Boilerplate setup (React + Context API)

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+//Suspense not used due to how experimental it is and my unfamiliarity with how to use it safely.
+
+function App() {
+	return <div className="App"></div>;
+}
+
+export default App;
